a6f0307e2363877ace17297e61ce87242fcbd1dd
nupersich <nupersich@gmail.com>
2023-10-19 16:41:05 +0300

    Исправление замечаний 0.1: Почистил код, импорты. Спрятал реализацию Hibernate из 1.1.5

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,29 +1,31 @@
 package jm.task.core.jdbc.util;
+import jm.task.core.jdbc.model.User;
 
-import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
 
 import java.sql.Connection;
+import org.hibernate.SessionFactory;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
 
 
-    private static Connection connection;
+    private Connection connection;
 
-    private static SessionFactory factory;
+    private SessionFactory factory;
 
-    private final static String URL = "jdbc:mysql://localhost:3306/mydatabase";
-    private final static String USERNAME = "root";
-    private final static String PASSWORD = "Terminatoratm123.";
+    private final String URL = "jdbc:mysql://localhost:3306/mydatabase";
+    private final String USERNAME = "root";
+    private final String PASSWORD = "Terminatoratm123.";
 
-    public static Connection getConnection () {
-//        if (!connection.isClosed()) {
-//            System.out.println("Connection has been made");
-//            return connection;
-//        } else {
+    public Connection getConnection () {
         try {
-            if (connection != null && !connection.isClosed()) {
+            if (!connection.isClosed() && connection != null) {
                 System.out.println("Connection has been made");
                 return connection;
             } else {
@@ -32,38 +34,38 @@ public class Util {
                 return connection;
             }
         } catch (SQLException e) {
+            System.out.println("Failed: Сonnection has NOT been established");
             e.printStackTrace();
         }
-        System.out.println("Failed: Сonnection has NOT been established");
         return connection;
     }
 
-//    public static SessionFactory getFactory () {
-//        if (factory == null) {
-//            try {
-//                Configuration config = new Configuration();
-//
-//                Properties settings = new Properties();
-//                settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-//                settings.put(Environment.URL, "jdbc:mysql://localhost:3306/mydatabase");
-//                settings.put(Environment.USER, "root");
-//                settings.put(Environment.PASS, "Terminatoratm123.");
-//                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
-//                settings.put(Environment.HBM2DDL_AUTO, "create-drop");
-//                settings.put(Environment.SHOW_SQL, "true");
-//                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
-//
-//                config.setProperties(settings);
-//                config.addAnnotatedClass(User.class);
-//                StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(config.getProperties()).build();
-//
-//                factory = config.buildSessionFactory(serviceRegistry);
-//            } catch (Exception exception) {
-//                System.out.println("Failed: GETFACTORY method has NOT been established");
-//                exception.printStackTrace();
-//            }
-//        }
-//        System.out.println("GETFACTORY method: Factory was obtained");
-//        return factory;
-//    }
+    public SessionFactory getFactory () {
+        if (factory == null) {
+            try {
+                Configuration config = new Configuration();
+
+                Properties settings = new Properties();
+                settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+                settings.put(Environment.URL, "jdbc:mysql://localhost:3306/mydatabase");
+                settings.put(Environment.USER, "root");
+                settings.put(Environment.PASS, "Terminatoratm123.");
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+                settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+                settings.put(Environment.SHOW_SQL, "true");
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+
+                config.setProperties(settings);
+                config.addAnnotatedClass(User.class);
+                StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(config.getProperties()).build();
+
+                factory = config.buildSessionFactory(serviceRegistry);
+            } catch (Exception exception) {
+                System.out.println("Failed: GETFACTORY method has NOT been established");
+                exception.printStackTrace();
+            }
+        }
+        System.out.println("GETFACTORY method has been established");
+        return factory;
+    }
 }
